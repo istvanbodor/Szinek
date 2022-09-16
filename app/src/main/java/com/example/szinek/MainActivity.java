97f7b7f8e536d7a78427819cb0622b93fec8149e
Bodor István <istvanbodor19@gmail.com>
2022-09-16 09:18:47 +0200

    Komponens inicializálása

--- app/src/main/java/com/example/szinek/MainActivity.java
+++ app/src/main/java/com/example/szinek/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.szinek;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private RelativeLayout layout;
+    private TextView textSzin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
     }
+    private void init()
+    {
+        layout = findViewById(R.id.layout);
+        textSzin = findViewById(R.id.textSzin);
+
+
+
+    }
+
+
 }
