22a3f188e3a5bb0ef3b6bc9798bee9423fde8d7a
Bodor István <istvanbodor19@gmail.com>
2022-09-16 09:31:57 +0200

    kesz

--- app/src/main/java/com/example/szinek/MainActivity.java
+++ app/src/main/java/com/example/szinek/MainActivity.java
@@ -2,15 +2,20 @@ package com.example.szinek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private RelativeLayout layout;
     private TextView textSzin;
+    private Random rnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +23,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         init();
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int red = rnd.nextInt(256);
+                int green = rnd.nextInt(256);
+                int blue = rnd.nextInt(256);
+
+
+                layout.setBackgroundColor(Color.rgb(red, green, blue));
+                String szinSzoveges = String.format("(%d,%d,%d)", red, green, blue);
+                textSzin.setText(szinSzoveges);
+            }
+        });
 
     }
-    private void init()
-    {
+
+    private void init() {
+        rnd = new Random();
         layout = findViewById(R.id.layout);
         textSzin = findViewById(R.id.textSzin);
-
 
 
     }
